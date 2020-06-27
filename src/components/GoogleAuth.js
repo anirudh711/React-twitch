979badceb132638707cad74e21c762d44c6396fb
Anirudh Madhavan <anirudhragavender@gmail.com>
2020-06-27 15:37:12 +0530

    redux form init

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -17,12 +17,12 @@ class GoogleAuth extends React.Component{
     }
     onAuthChange=(isSignedIn)=>{
         if(isSignedIn)
-            this.props.signIn();
+            this.props.signIn(this.auth.currentUser.get().getId());
         else
             this.props.signOut();
     };
     onSignInClick =()=>{
-        this.auth.signIn(this.auth.currentUser.get().getId()); 
+        this.auth.signIn(); 
     }
     onSignOutClick =()=>{
         this.auth.signOut();
